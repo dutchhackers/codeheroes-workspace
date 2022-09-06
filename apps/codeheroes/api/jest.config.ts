7a5521587fd888c24060a160d7a35ee99bd71f13
Michael Schilling <mschilling@move4mobile.com>
2022-09-06 21:09:12 +0200

    Initial setup

---
+++ apps/codeheroes/api/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'codeheroes-api',
+
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/codeheroes-api',
+  preset: '../../../jest.preset.js',
+};
---
+++ libs/codeheroes/auth/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'codeheroes-auth',
+
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/codeheroes/auth',
+  preset: '../../../jest.preset.js',
+};
